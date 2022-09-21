b8f28b37c9d74895ea357fff08b093ad2001d18b
Valentin Jadot <valentinjadot@gmail.com>
2022-09-21 15:15:59 -0300

    feat(app) : change title

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ function App() {
   return (
     <>
       <h1>🚣‍♂️</h1>
-      <h1>Lindos dias para andar en kayak</h1>
+      <h1>¿Cómo está el día para andar en kayak?</h1>
 
       {!days && <LoadSection />}
 
